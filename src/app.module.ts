e666e97db1a0246312787fe6a9ee45abbf98cc6d
hebertcisco <hebertcisco@outlook.com>
2024-12-04 23:22:33 -0300

    build: update crypto import path and add node types to tsconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from 'nestjs-undici';
-import crypto from 'node:crypto';
+import crypto from 'crypto';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
